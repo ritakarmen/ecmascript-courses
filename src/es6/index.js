071bd9c273f533c6e2662ca1c4f689c276b0864a
John Doe <rita.ortiz@glr.pe>
2021-10-05 03:26:34 -0500

    clases practicando

--- src/es6/index.js
+++ src/es6/index.js
@@ -96,3 +96,19 @@ hellopromesa()
 .then(response => console.log(response))
 .catch(error=>console.log(error));
 
+// clases
+
+class calculator{
+    constructor(){
+        this.valueA = 0;
+        this.valueB = 0;
+        }
+    sum(valueA,valueB){
+        this.valueA = valueA;
+        this.valueB = valueB;
+
+        return this.valueA + this.valueB;
+    }
+}
+ const calc = new calculator();
+ console.log(calc.sum(5,3));
